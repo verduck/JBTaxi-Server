3570302f006a51497ccb3ecb1b3f5e529b7648cd
cwooder <choijy8816@gmail.com>
2021-11-21 15:36:40 +0900

    Add pass buy

--- src/main/java/com/bootcamp/jbtaxi/controller/PassController.java
+++ src/main/java/com/bootcamp/jbtaxi/controller/PassController.java
@@ -1,11 +1,14 @@
 package com.bootcamp.jbtaxi.controller;
 
+import java.util.HashMap;
 import java.util.List;
 
 import com.bootcamp.jbtaxi.domain.Pass;
+import com.bootcamp.jbtaxi.dto.UserDTO;
 import com.bootcamp.jbtaxi.service.PassService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -20,5 +23,10 @@ public class PassController {
   @RequestMapping(value = "/all", method = RequestMethod.GET)
   public @ResponseBody List<Pass> getAll() {
     return passService.getAll();
+  }
+
+  @RequestMapping(value = "/buy", method = RequestMethod.POST)
+  public @ResponseBody UserDTO buy(@RequestBody HashMap<String, Object> request) {
+    return passService.buy(request);
   }
 }
--- src/main/java/com/bootcamp/jbtaxi/service/PassService.java
+++ src/main/java/com/bootcamp/jbtaxi/service/PassService.java
@@ -1,19 +1,41 @@
 package com.bootcamp.jbtaxi.service;
 
+import java.util.HashMap;
 import java.util.List;
 
 import com.bootcamp.jbtaxi.domain.Pass;
+import com.bootcamp.jbtaxi.domain.User;
+import com.bootcamp.jbtaxi.dto.UserDTO;
 import com.bootcamp.jbtaxi.repository.PassRepository;
+import com.bootcamp.jbtaxi.repository.UserRepository;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PassService {
   @Autowired
+  private UserRepository userRepository;
+  @Autowired
   private PassRepository passRepository;
+  @Autowired
+  private ModelMapper modelMapper;
   
   public List<Pass> getAll() {
     return passRepository.findAll();
+  }
+
+  public UserDTO buy(HashMap<String, Object> request) {
+    Integer userId = (Integer) request.get("userId");
+    Integer passId = (Integer) request.get("passId");
+
+    User user = userRepository.getById(userId);
+    Pass pass = passRepository.getById(passId);
+
+    user.setPass(user.getPass() + pass.getDistance());
+    user = userRepository.save(user);
+
+    return modelMapper.map(user, UserDTO.class);
   }
 }
